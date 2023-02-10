04447ef6d941e6e709eed0a2dd5f800f87c3389d
raihan tajdid <raihantajdid@gmail.com>
2023-02-10 23:09:46 +0600

    standard-deviation

---
+++ standard-deviation.py
@@ -0,0 +1,14 @@
+import statistics
+import math
+import os
+import random
+import re
+import sys
+
+
+def stdDev(arr):
+    Sum = 0
+    me = statistics.mean(arr)
+    for i in arr:
+        Sum += abs(i-me)**2
+    print(math.sqrt(Sum/(len(arr))))
