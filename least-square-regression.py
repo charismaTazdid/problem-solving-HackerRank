83f29f6d29b4ec8221b4482f222b1a55d4f59b69
raihan tajdid <raihantajdid@gmail.com>
2023-02-19 20:04:06 +0600

    least-square-regression-line

--- least-square-regression.py
+++ least-square-regression.py
@@ -1,1 +1,25 @@
 # Probelm: https://www.hackerrank.com/challenges/s10-least-square-regression-line/problem?isFullScreen=true
+
+n = 5
+x = []
+y = []
+for i in range(0, n):
+    a, b = map(int, input().split())
+    x.append(a)
+    y.append(b)
+sumX = sum(x)
+sumY = sum(y)
+x_ = sumX/n
+y_ = sumY/n
+xy = 0
+x2 = 0
+for i in range(0, n):
+    xy += x[i] * y[i]
+    x2 += x[i] ** 2
+
+b = (n * xy - sumX * sumY)/(n * x2 - sumX**2)
+a = y_ - b*x_
+
+X = 80
+Y = a + b*X
+print(round(Y, 3))
