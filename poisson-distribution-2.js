728fb89935aab1d015a3475f3a3189c7833579ee
raihan tajdid <raihantajdid@gmail.com>
2023-03-13 06:16:56 +0600

    Poisson Distribution 2

--- poisson-distribution-2.js
+++ poisson-distribution-2.js
@@ -1,1 +1,10 @@
 // Problem : https://www.hackerrank.com/challenges/s10-poisson-distribution-2/problem?isFullScreen=true
+
+
+function processData(input) {
+    //Enter your code here
+    let value = input.split(" ");
+    let ca = 160 + (40 * (Math.pow(parseFloat(value[0]), 2) + parseFloat(value[0])));
+    let cb = 128 + (40 * (Math.pow(parseFloat(value[1]), 2) + parseFloat(value[1])));
+    console.log(ca.toFixed(3), "\n" , cb.toFixed(3));
+} 
