69d4ab7eea85c75ec0a8dd0a98449c793081fe06
raihan tajdid <raihantajdid@gmail.com>
2023-03-13 06:24:38 +0600

    Poisson Distribution 2 HR

--- poisson-distribution-2.js
+++ poisson-distribution-2.js
@@ -2,9 +2,16 @@
 
 
 function processData(input) {
-    //Enter your code here
-    let value = input.split(" ");
-    let ca = 160 + (40 * (Math.pow(parseFloat(value[0]), 2) + parseFloat(value[0])));
-    let cb = 128 + (40 * (Math.pow(parseFloat(value[1]), 2) + parseFloat(value[1])));
-    console.log(ca.toFixed(3), "\n" , cb.toFixed(3));
-} 
+    // Split the input string into an array of strings
+    let inputArray = input.split(" ");
+
+    // Calculate the value of ca using the input values and a formula
+    let cost_of_a = 160 + (40 * (Math.pow(parseFloat(inputArray[0]), 2) + parseFloat(inputArray[0])));
+
+    // Calculate the value of cb using the input values and a formula
+    let cost_of_b = 128 + (40 * (Math.pow(parseFloat(inputArray[1]), 2) + parseFloat(inputArray[1])));
+
+    // Print the values of ca and cb with 3 decimal places
+    console.log(cost_of_a.toFixed(3), "\n" , cost_of_b.toFixed(3));
+}
+
