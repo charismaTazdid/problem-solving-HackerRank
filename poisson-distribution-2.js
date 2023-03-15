a11e6eb9625ca00ea79a2e6516cd95dd6ba01c21
raihan tajdid <raihantajdid@gmail.com>
2023-03-15 23:58:53 +0600

    poisson distribution

--- poisson-distribution-2.js
+++ poisson-distribution-2.js
@@ -2,16 +2,12 @@
 
 
 function processData(input) {
-    // Split the input string into an array of strings
+
     let inputArray = input.split(" ");
 
-    // Calculate the value of ca using the input values and a formula
     let cost_of_a = 160 + (40 * (Math.pow(parseFloat(inputArray[0]), 2) + parseFloat(inputArray[0])));
-
-    // Calculate the value of cb using the input values and a formula
     let cost_of_b = 128 + (40 * (Math.pow(parseFloat(inputArray[1]), 2) + parseFloat(inputArray[1])));
 
-    // Print the values of ca and cb with 3 decimal places
-    console.log(cost_of_a.toFixed(3), "\n" , cost_of_b.toFixed(3));
+    console.log(cost_of_a.toFixed(3), "\n", cost_of_b.toFixed(3));
 }
 
