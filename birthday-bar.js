9770bc74d19fe3c208ff1469443c94ef902ddc0b
raihan tajdid <raihantajdid@gmail.com>
2023-04-09 14:03:07 +0600

    birthday bar

---
+++ birthday-bar.js
@@ -0,0 +1,12 @@
+// Problem : https://www.hackerrank.com/challenges/the-birthday-bar/problem?isFullScreen=true
+
+function birthday(s, d, m) {
+    let res = 0
+    const sum = arr => arr.reduce((acc, el) => acc + el, 0)
+    for (let i = 0; i < s.length - m + 1; i++) {
+        if (sum(s.slice(i, i + m)) === d) {
+            res++
+        }
+    }
+    return res
+}
