f5d42e59fcc0f5918af78bf2fc72293d85482719
raihan tajdid <raihantajdid@gmail.com>
2023-06-13 23:37:03 +0600

    Super reduced with Regex

--- superReducedString.js
+++ superReducedString.js
@@ -1,1 +1,14 @@
 // Problem: https://www.hackerrank.com/challenges/reduced-string
+
+
+function superReducedString(s) {
+    // Write your code here
+    const regex = /(\w)\1/g;
+
+    if (regex.test(s)) {
+        s = s.replaceAll(regex, "");
+        return superReducedString(s);
+    }
+
+    return s || "Empty String";
+}
