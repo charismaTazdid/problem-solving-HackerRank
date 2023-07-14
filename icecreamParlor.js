351b043fdb8aae5afc4b4551ebcacb8e330a4f35
raihan tajdid <raihantajdid@gmail.com>
2023-07-14 22:53:28 +0600

    ice cream

---
+++ icecreamParlor.js
@@ -0,0 +1,14 @@
+// Problem: 
+
+function icecreamParlor(m, arr) {
+    // Write your code here
+    let results = {};
+    
+    for(let i = 0; i <= arr.length; i++){
+        if(results.hasOwnProperty(arr[i])) {
+            return [results[arr[i]] + 1, i + 1]
+        }
+
+        results[m - arr[i]] = i;
+    }
+}
