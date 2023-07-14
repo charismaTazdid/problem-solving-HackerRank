122fc6c57318344173a284aa17e806bf743f39ae
raihan tajdid <raihantajdid@gmail.com>
2023-07-14 22:53:56 +0600

    icecream-parlor

--- icecreamParlor.js
+++ icecreamParlor.js
@@ -1,11 +1,11 @@
-// Problem: 
+// Problem: https://www.hackerrank.com/challenges/icecream-parlor/
 
 function icecreamParlor(m, arr) {
     // Write your code here
     let results = {};
-    
-    for(let i = 0; i <= arr.length; i++){
-        if(results.hasOwnProperty(arr[i])) {
+
+    for (let i = 0; i <= arr.length; i++) {
+        if (results.hasOwnProperty(arr[i])) {
             return [results[arr[i]] + 1, i + 1]
         }
 
