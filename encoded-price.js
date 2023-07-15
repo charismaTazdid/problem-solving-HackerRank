7efd7675c9df7b707109caf4f76abcd2d1238cb0
raihan tajdid <raihantajdid@gmail.com>
2023-07-15 23:28:30 +0600

    Decode Price

--- encoded-price.js
+++ encoded-price.js
@@ -39,3 +39,50 @@ const encodePrices = (buyingPrice, sellingPrice, dressCode) => {
 
 const test = encodePrices(398, 467, "SHI");
 console.log(test)
+
+
+
+// Price Decode
+export const decodePrice = (str) => {
+    const codeMap = {
+        'X': '0', 'Y': '1', 'W': '2', 'A': '3',
+        'T': '4', 'C': '5', 'S': '6',
+        'Q': '7', 'J': '8', 'P': '9'
+    };
+
+    const dressCodeLength = 7;
+    const encodedDigits = str.slice(dressCodeLength);
+    const decodedDigits = encodedDigits
+        .split('')
+        .map(encodedDigit => codeMap[encodedDigit])
+        .join('');
+
+    const arr = str.slice(0, 6).split("");
+    const dressCode = arr.filter((char) => char !== "0" && char !== "1").join("");
+    return { dressCode: dressCode, price: Number(decodedDigits) };
+};
+
+export const decodePrices = (encodedBuyingPrice, encodedSellingPrice) => {
+    const dressCodeLength = 3;
+    const buyingPrice = decodePrice(encodedBuyingPrice.slice(dressCodeLength));
+    const sellingPrice = decodePrice(encodedSellingPrice.slice(dressCodeLength));
+
+    return {
+        buyingPrice,
+        sellingPrice
+    };
+};
+
+// const encodedPrices = encodePrices(3980, 4675, 'SHI');
+// console.log(encodedPrices);
+// Output: { buyingPrice: 'SHI11010APJX', sellingPrice: 'SHI10100TSQC' }
+// const decodedPrices = decodePrices('SHI11010APJX', 'SHI10100TSQC');
+// Output: { buyingPrice: 3980, sellingPrice: 4675 }
+
+
+// Helper: Decode DressCode
+const findDressCode = (str) => {
+    const arr = str.slice(0, 6).split("");
+    const dressCode = arr.filter((char) => char !== "0" && char !== "1").join("");
+    return dressCode;
+};
