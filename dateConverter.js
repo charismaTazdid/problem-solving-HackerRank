a74364c84639d2693edbe2631d713739a501f6b3
raihan tajdid <raihantajdid@gmail.com>
2023-10-04 20:31:16 +0600

    formate date time and day

---
+++ dateConverter.js
@@ -0,0 +1,34 @@
+// It was an Interview task
+
+export const getBanglaDayName = (day) => {
+    const banglaDaysName = {
+        Saturday: "শনিবার",
+        Sunday: "রবিবার",
+        Monday: "সোমবার",
+        Tuesday: "মঙ্গলবার",
+        Wednesday: "বুধবার",
+        wednesday: "বুধবার",
+        Thursday: "বৃহস্পতিবার",
+        Friday: "শুক্রবার",
+        Daily: "দৈনিক",
+    };
+
+    return banglaDaysName[day];
+};
+
+export const getFormattedDateTime = (dateTime) => {
+    const date = new Date(dateTime);
+    const day = date.getDate();
+    const month = date.toLocaleString("default", { month: "short" });
+    const formattedTime = date.toLocaleString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        timeZone: "UTC",
+    });
+
+    return {
+        day,
+        month,
+        formattedTime,
+    };
+};
